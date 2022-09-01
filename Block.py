b8a5d0ede73b5eaf1c391d3e5d6a1c5031b9b5b4
shunae95 <112442940+shunae95@users.noreply.github.com>
2022-08-31 19:57:08 -0500

    Add files via upload
    Added in project transaction block and main class. Also added in empty block.py file and sample json file that was output from the current code

---
+++ Block.py
@@ -0,0 +1,3 @@
+class Block:
+    def __init__(self) -> None:
+        pass
--- Main.py
+++ Main.py
@@ -1,23 +1,9 @@
-import time
-import datetime
 import json
 import hashlib
 import os
+from Transaction import Transaction
 
-class Transaction:
-    def createTransaction(self):
-        currentTime = datetime.datetime.now()
-        self.time = datetime.datetime.timestamp(currentTime)*1000
-        self.sender = input("Sender: ")
-        self.recipient = input("Recipient: ")
-        self.amount = float(input("Enter transaction amount: "))
-
-        return self.time, self.sender, self.recipient, self.amount
-        
-
-
-
-def main(self):
+def main():
     
     transactions = []
 
@@ -27,13 +13,10 @@ def main(self):
         if addTransaction == "y" or addTransaction == "yes":
             newTransaction = Transaction()
             transaction = newTransaction.createTransaction()
-            print(transaction)
             transactions.append(transaction)
-            print(transactions)
         
         elif addTransaction != "y":
-            transactionJson = json.dumps(transactions)
-        
+            transactionJson = json.dumps(transactions)        
             with open("transactions.json","w") as outfile:
                 json.dump(transactionJson, outfile)
         
@@ -41,10 +24,9 @@ def main(self):
             with open("transactions.json","rb") as f:
                 file = f.read() 
                 hash_text = hashlib.sha256(file).hexdigest()
-                print(hash_text)
 
             os.rename('transactions.json', f'{hash_text}.json')
             return    
      
 if __name__=="__main__":
-    main(Transaction())
+    main()
